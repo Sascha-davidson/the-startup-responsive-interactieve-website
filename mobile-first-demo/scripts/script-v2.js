aabdbdfb2a73043a4102cf71a09ed6451056e15c
Sascha-davidson <saschadavidson1@gmail.com>
2023-01-25 11:14:22 +0100

    svg animation

--- mobile-first-demo/scripts/script-v2.js
+++ mobile-first-demo/scripts/script-v2.js
@@ -30,6 +30,7 @@
 
 
 var pos = document.documentElement;
+
 pos.addEventListener("mousemove", e =>{
     pos.style.setProperty('--x', e.clientX + "px")
     pos.style.setProperty('--y', e.clientY + "px")
