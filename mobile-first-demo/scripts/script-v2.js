b10594691b488d944e9cdb252080d64da74c660e
Sascha-davidson <saschadavidson1@gmail.com>
2023-01-25 11:01:04 +0100

    vermindering content + eerste mouse track

--- mobile-first-demo/scripts/script-v2.js
+++ mobile-first-demo/scripts/script-v2.js
@@ -1,31 +1,38 @@
-let temp = document.getElementById('temp');
-let cursorClip = document.querySelector("div.front");
+// let temp = document.getElementById('temp');
+// let cursorClip = document.querySelector("div.front");
 
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
+// let screenWidth = window.innerWidth;
+// let screenHeight = window.innerHeight;
 
-let x = 0;
-let y = 0;
+// let x = 0;
+// let y = 0;
 
-let xPercentage = 0;
-let yPercentage = 0;
+// let xPercentage = 0;
+// let yPercentage = 0;
 
-cursorClip.style.clipPath = `circle(6% at ${x} ${y})` ;
+// cursorClip.style.clipPath = `circle(6% at ${x} ${y})` ;
 
-window.addEventListener("mousemove", (event)=>{
+// window.addEventListener("mousemove", (event)=>{
 
-    x = event.clientX;
-    y = event.clientY;
+//     x = event.clientX;
+//     y = event.clientY;
 
-    xPercentage = 100 / screenWidth * x;
-    yPercentage = 100 / screenHeight * y;
+//     xPercentage = 100 / screenWidth * x;
+//     yPercentage = 100 / screenHeight * y;
 
-    let xPercentageString = xPercentage.toString() + "%";
-    let yPercentageString = yPercentage.toString() + "%";
+//     let xPercentageString = xPercentage.toString() + "%";
+//     let yPercentageString = yPercentage.toString() + "%";
 
-    cursorClip.style.clipPath = `circle(6% at ${xPercentageString} ${yPercentageString})` ;
+//     cursorClip.style.clipPath = `circle(6% at ${xPercentageString} ${yPercentageString})` ;
 
-    console.log(x);
+//     console.log(x);
+// })
+
+
+var pos = document.documentElement;
+pos.addEventListener("mousemove", e =>{
+    pos.style.setProperty('--x', e.clientX + "px")
+    pos.style.setProperty('--y', e.clientY + "px")
 })
 
 
